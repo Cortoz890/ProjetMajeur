93da4d8eab35ccd87b7d61ef3f92031d1d643444
Louis-Joachin Messié <louis-joachin.messie@cpe.fr>
2022-06-01 10:48:11 +0200

    Affichage map

---
+++ Front/script.js
@@ -0,0 +1,35 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoibGpldGJhcHRpc3RlIiwiYSI6ImNsM3ZhYTNlMzBwM3Izam5wOGNycGoxdG0ifQ.NWzdwquyLVm5ZjHe3jrQbQ';
+var map = L.map('map').setView([45.75,4.83], 15);
+
+
+L.tileLayer(
+    'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=' + L.mapbox.accessToken, {
+        tileSize: 512,
+        zoomOffset: -1,
+        attribution: '© <a href="https://www.mapbox.com/contribute/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+var marker = L.marker([45.75, 4.83]).addTo(map);
+
+generate()
+function generate(){
+
+    const GET_URL="http://vps.cpe-sn.fr:8081/fire"; 
+    let context =   {
+                        method: 'GET'
+                    };
+        
+    fetch(GET_URL,context)
+        .then(response => response.json())
+            .then(response => callback(response))
+            .catch(error => err_callback(error));
+}
+
+function callback(response){
+    console.log(response.value)
+}
+
+function err_callback(error){
+    console.log(error);
+}
+

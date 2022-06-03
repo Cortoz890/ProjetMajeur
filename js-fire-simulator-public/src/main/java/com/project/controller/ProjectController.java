af878b2ee0461bfee4aea2d71486288cf73783c8
Baptiste <baptiste.boiteux@cpe.fr>
2022-06-03 13:57:49 +0200

    retour ihm

--- js-fire-simulator-public/src/main/java/com/project/controller/ProjectController.java
+++ js-fire-simulator-public/src/main/java/com/project/controller/ProjectController.java
@@ -42,11 +42,11 @@ public class ProjectController {
 	
 	
 	  
-  	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
-  		public String index(Model model) {  
-  		return "index";
- 
-  	}
+//  	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+//  		public String index(Model model) {  
+//  		return "index";
+// 
+//  	}
 
   	@RequestMapping(value = { "/facility" }, method = RequestMethod.GET)
 		public StringBuffer getAllFacilities(Model model) throws IOException {  

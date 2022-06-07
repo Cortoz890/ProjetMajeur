a499c75a3790b0c712d0801bb9810b0f2162f86a
Baptiste <baptiste.boiteux@cpe.fr>
2022-06-07 13:53:30 +0200

    deleteVehicle

--- js-fire-simulator-public/src/main/resources/static/script.js
+++ js-fire-simulator-public/src/main/resources/static/script.js
@@ -182,7 +182,7 @@ function updateCaserneList(){
 
 function supprimerCamion(){
     if(confirm("Voulez vous supprimer le véhicule "+document.getElementById("del_camion").value+" ?")){
-        const DEL_URL= URL_base +"/camion/"+document.getElementById("del_camion").value;
+        const DEL_URL= URL_base +"/deleteVehicle/"+document.getElementById("del_camion").value;
         let context =   {
                             method: 'DEL'
                         };

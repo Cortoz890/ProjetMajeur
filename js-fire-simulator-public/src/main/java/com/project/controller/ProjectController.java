184e121ba1e31d020398bd0f92a6e1073f9192fb
Louis-Joachin Messié <louis-joachin.messie@cpe.fr>
2022-06-07 08:32:11 +0200

    changements corto

--- js-fire-simulator-public/src/main/java/com/project/controller/ProjectController.java
+++ js-fire-simulator-public/src/main/java/com/project/controller/ProjectController.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 import com.project.model.dto.FireDto;
+import com.project.model.dto.VehicleDto;
 
 
 @RestController 
@@ -130,32 +132,29 @@ public class ProjectController {
 	}
   	
   	
-		public double[] getOneFire() throws IOException{  
-  		//int fireId = 0;
-  		double[] coordinates= new double[3];
+  	public double getOneFire(int fireId) throws IOException{ 
+  		double fireIntensity = 0;
+  		
   		try {
-  				URL url = new URL("http://vps.cpe-sn.fr:8081/fire/");
+  				URL url = new URL("http://vps.cpe-sn.fr:8081/fire/"+fireId);
   				HttpURLConnection con = (HttpURLConnection) url.openConnection();
   				con.setRequestMethod("GET");
   				
   				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-  				System.out.println(in);
+  		
   				String inputLine;
   				StringBuffer content = new StringBuffer();
   				while ((inputLine = in.readLine()) != null) {
-  					System.out.println(inputLine);
+  				
   					content.append(inputLine);
   					String[] words = inputLine.split(",");
   					
   					for(int i = 0; i < words.length; i++) {
   						words[i] = words[i].split(":")[1];
-	  					//System.out.println(words[i]);
   					}
   					words[5] = words[5].substring(0,words[5].length()-1);
   					
-  					coordinates[0] = Double.parseDouble(words[5]);
-  					coordinates[1] = Double.parseDouble(words[4]);
-  					coordinates[2] = Double.parseDouble(words[2]);
+  					fireIntensity = Double.parseDouble(words[2]);  
   					//fireId = Integer.parseInt(words[0]);  
   					//System.out.println(words[0]);
   				}
@@ -164,40 +163,117 @@ public class ProjectController {
   			} catch (MalformedURLException e) {
   				
   			}
-  			return coordinates;
-			
+		return fireIntensity;
+  			
+	}
+
+		public double[] getAllFire() throws IOException{  
+			  
+			  double[] coordinates= new double[4];
+			  try {
+			  URL url = new URL("http://vps.cpe-sn.fr:8081/fire/");
+			  HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			  con.setRequestMethod("GET");
+			  
+			  BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			  
+			  String inputLine;
+			  StringBuffer content = new StringBuffer();
+			  while ((inputLine = in.readLine()) != null) {
+			  
+			  content.append(inputLine);
+			  String[] words = inputLine.split(",");
+			  
+			  for(int i = 0; i < words.length; i++) {
+			  words[i] = words[i].split(":")[1];
+			  
+			  }
+			  words[5] = words[5].substring(0,words[5].length()-1);
+			  
+			  coordinates[0] = Double.parseDouble(words[5]);//lon
+			  coordinates[1] = Double.parseDouble(words[4]);//lat
+			  coordinates[2] = Double.parseDouble(words[2]);//intensity
+			  coordinates[3] = Double.parseDouble(words[0]);//id
+			  
+			  }
+			  in.close();
+			  
+			  } catch (MalformedURLException e) {
+			  
+			  }
+			  return coordinates;
+
+			}
+
+		
+		
+		@RequestMapping(value = { "/moveVehicle/{teamuuid}" }, method = RequestMethod.GET)
+		public double updateVehicle(@PathVariable String teamuuid, @PathVariable int id, @RequestBody VehicleDto vehicle) throws IOException, InterruptedException { 
+		  double[] coordinates = getAllFire();
+		  
+		  double fireIntensity = getOneFire((int)coordinates[3]);
+		  
+		  coordinates = getAllFire();
+		JSONObject json = new JSONObject();
+		  json.put("crewMember", vehicle.getCrewMember());
+		  json.put("facilityRefID", 82);
+		  json.put("fuel", vehicle.getFuel());
+		  json.put("id", id);
+		  json.put("lat", coordinates[0]);
+		  json.put("liquidQuantity", vehicle.getLiquidQuantity());
+		  json.put("liquidType", vehicle.getLiquidType());
+		  json.put("lon", coordinates[1]);
+		  json.put("type", vehicle.getType());
+		  
+		  HttpPost post = new HttpPost("http://vps.cpe-sn.fr:8081/vehicle/"+teamuuid); 
+
+		        post.setEntity(new StringEntity(json.toString(),ContentType.APPLICATION_JSON)); 
+
+		        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+		             CloseableHttpResponse response = httpClient.execute(post)) { 
+
+		            //System.out.println(EntityUtils.toString(response.getEntity()));
+		        }
+		  
+		  
+		  while(true) {
+		  
+		  if(fireIntensity > 0) {  
+		  
+		  Thread.sleep(1000);
+		  System.out.println(fireIntensity+" "+coordinates[3]);
+		  }
+		  
+		  else {
+		  Thread.sleep(5000);
+		  coordinates = getAllFire();
+		  JSONObject json2 = new JSONObject();
+		  json.put("crewMember", vehicle.getCrewMember());
+		    json.put("facilityRefID", 82);
+		    json.put("fuel", vehicle.getFuel());
+		    json.put("id", vehicle.getId());
+		    json.put("lat", coordinates[0]);
+		    json.put("liquidQuantity", vehicle.getLiquidQuantity());
+		    json.put("liquidType", vehicle.getLiquidType());
+		    json.put("lon", coordinates[1]);
+		    json.put("type", vehicle.getType());
+		    
+		    HttpPost post2 = new HttpPost("http://vps.cpe-sn.fr:8081/vehicle/"+teamuuid);
+
+		          post2.setEntity(new StringEntity(json2.toString(),ContentType.APPLICATION_JSON));
+
+		          try (CloseableHttpClient httpClient = HttpClients.createDefault();
+		               CloseableHttpResponse response = httpClient.execute(post2)) {
+
+		              //System.out.println(EntityUtils.toString(response.getEntity()));
+		          }
+		  }
+		  fireIntensity = getOneFire((int)coordinates[3]);
+		  }
 }
 
 
-  	@RequestMapping(value = { "/moveVehicle/{teamuuid}/{id}" }, method = RequestMethod.GET)
-		public void updateVehicle(@PathVariable String teamuuid, @PathVariable int id) throws IOException, InterruptedException { 
-  		double[] coordinates = getOneFire();
-  		while(coordinates[2] > 0) {
-  		
-  		JSONObject json = new JSONObject();
-  		json.put("crewMember", 5);
-  		json.put("facilityRefID", 82);
-  		json.put("fuel", 100);
-  		json.put("id", id);
-  		json.put("lat", coordinates[0]);
-  		json.put("liquidQuantity", 100);
-  		json.put("liquidType", "ALL");
-  		json.put("lon", coordinates[1]);
-  		json.put("type", "CAR");
-  	
-  		HttpPost post = new HttpPost("http://vps.cpe-sn.fr:8081/vehicle/"+teamuuid);
 
-        post.setEntity(new StringEntity(json.toString(),ContentType.APPLICATION_JSON));
-
-        try (CloseableHttpClient httpClient = HttpClients.createDefault();
-             CloseableHttpResponse response = httpClient.execute(post)) {
-
-            System.out.println(EntityUtils.toString(response.getEntity()));
-        }
-        
-        coordinates = getOneFire();
-  		}
-}
   	
   	@RequestMapping(value = { "/vehicle/{teamuuid}/{id}" }, method = RequestMethod.DELETE)
 		public String delVehicle(@PathVariable int teamuuid, @PathVariable int id) {  
@@ -283,10 +359,12 @@ public class ProjectController {
 	@RequestMapping(value="/moveLine/{teamuuid}/{id}", method=RequestMethod.GET)
 	public void moveInLine(@PathVariable int id,@PathVariable String teamuuid) throws IOException, InterruptedException {
 		double[] vehiculeCoordinates = getVehicle(id);
-		double[] fireCoordinates = getOneFire();
 		
-		double x = vehiculeCoordinates[0] - fireCoordinates[0];
-		double y = vehiculeCoordinates[1] - fireCoordinates[1];
+		double[] fireCoordinates = getAllFire();
+		  
+		
+		double x = vehiculeCoordinates[0] - fireCoordinates[1];
+		double y = vehiculeCoordinates[1] - fireCoordinates[0];
 		
 		double rapport = y/x;
 		
